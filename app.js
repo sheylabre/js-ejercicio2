ca1b3efab39c432de13f023e6479e134f0d5d970
sheylabre <sheylabrena@hotmail.com>
2016-08-25 15:25:27 -0500

    ejercico2

---
+++ app.js
@@ -0,0 +1,8 @@
+var num1 = parseInt(prompt("numero1"));
+var num2 = parseInt(prompt("numero2"));
+var num3 = parseInt(prompt("numero3"));
+
+var resul = (num1 + num2 + num3)/3;
+document.write("El promedio es:" + resul) ;
+
+
